a675a5fb8645f661ab0ddf4cc588013230270541
Venkata Vijaya Rama Raju Mandapati <vn520794@dal.ca>
2022-06-22 18:53:20 -0300

    class creation

---
+++ src/main/java/com/dal/drplus/model/Payment.java
@@ -0,0 +1,25 @@
+package com.dal.drplus.model;
+
+public class Payment {
+    private String paymentId;
+    private String billId;
+    private String paymentType;
+    private String paymentDetails;
+    private boolean paymentStatusEnabled;
+
+    public void addPayment(){
+
+    }
+
+    public void getPayment(){
+
+    }
+
+    public void updatePayment(){
+
+    }
+
+    public void revertPaymentAppointmentCancelled(){
+
+    }
+}
---
+++ src/main/java/com/dal/drplus/model/Report.java
@@ -0,0 +1,48 @@
+package com.dal.drplus.model;
+
+public class Report {
+    private String reportId;
+    private String patientId;
+    private String doctorId;
+    private String reportDetails;
+
+    public String getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(String reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getReportDetails() {
+        return reportDetails;
+    }
+
+    public void setReportDetails(String reportDetails) {
+        this.reportDetails = reportDetails;
+    }
+
+    public void uploadReport(){
+
+    }
+
+    public void deleteReport(){
+
+    }
+}
