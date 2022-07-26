bdc36240909dec761b924c095b66ca66343931f2
Parul Raich <pr678267@dal.ca>
2022-07-26 01:59:55 -0300

    changed the port

--- src/main/java/com/dal/drplus/controller/DrPlusController.java
+++ src/main/java/com/dal/drplus/controller/DrPlusController.java
@@ -1,13 +1,14 @@
 package com.dal.drplus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class DrPlusController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String WelcomePage(){
         return "home";
     }
